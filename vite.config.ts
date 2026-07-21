0de6462f2a06814afeb88381d524592cfaf48c01
karthik4923 <93390048+karthik4923@users.noreply.github.com>
2026-07-21 17:29:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
-import plugin from '@vitejs/plugin-vue';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin()],
-    server: {
-        port: 55867,
-    }
+    plugins: [vue()],
+    base: '/Whatsappclone/'
 })
